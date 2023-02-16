6df52d912d1d5d6ab73c7c629fd176226653e479
doubra50 <doubraloveworld@yahoo.com>
2023-02-16 14:37:03 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
+/**
  * main- print a line using puts function
  *
  * Return:0
  */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-	retun (0);
+puts("\"Programming is like building a multilingual puzzle");
+retun(0);
 }
