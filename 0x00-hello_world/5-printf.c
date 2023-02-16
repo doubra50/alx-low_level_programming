f036225622ddc17e72a6aa8cc4efba90ab0aaa24
doubra50 <doubraloveworld@yahoo.com>
2023-02-16 14:22:30 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main- print a line using printf function
+ *
+ * Return:0
+ */
+int main(void)
+{
+        printf("with proper grammar\, but the outcome is a piece of art\,");
+        retun (0);
+}
