d4291188258da68d8a81a31b8f81dee4dfb70f22
doubra50 <doubraloveworld@yahoo.com>
2023-02-17 20:43:14 -0800

    latest

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -22,8 +22,8 @@ int main(void)
 
 				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 				continue;
-			putchar(',');
-          		putchart (' ');
+	          		putchar(',');
+          	        	putchar(' ');
 
 			}
 		}
