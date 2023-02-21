fe435a69d2fde55b356f147cbaa58a79415d79c6
doubra50 <doubraloveworld@yahoo.com>
2023-02-21 14:31:17 -0800

    latest

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,13 +2,18 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
+ * a program that prints _putchar
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: 0
  */
-int _putchar(char c)
+int main()
 {
-	return (write(1, &c, 1));
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('\n');
+
+	return 0;
 }
 
