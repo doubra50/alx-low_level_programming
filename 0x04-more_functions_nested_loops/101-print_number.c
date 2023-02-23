9f1100406a1e95d6c5e36a9e182921ee7cf9486b
doubra50 <doubraloveworld@yahoo.com>
2023-02-23 14:02:07 -0800

    correction of error

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,4 @@
 #include "main.h"
-void print_integer(int m);
 
 /**
  * print_number - a function that prints an integer.
@@ -17,20 +16,4 @@ void print_number(int n)
 	}
 	else
 		print_integer(n);
-}
-
-/**
- * print_integer - A function to priting n
- * @m: an input unsigned integer
- * Return: Nothing
- */
-void print_integer(int m)
-{
-	int i = 1000000000;
-
-	for (; i >= 1; i /= 10)
-		if (m / i != 0)
-		{
-			_putchar((m / i) % 10 + '0');
-		}
 }
